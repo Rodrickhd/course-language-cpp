5bb774bd5d30372c6d1a769b72c66f201b91d74e
Rodrickhd <brodrigo87@gmail.com>
2020-06-09 15:27:12 +0000

    Aula de Manipulação de dados

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
+/* Aula de Manipulação de dados */
 #include <iostream>
 
+//using namespace std;
+
 int main() {
-  std::cout << "My Hello World !\n";
+  /* ler valor e atribui a num1 */
+  std::cout << "Informe um número: \n";
+  int num1 = 0;
+  std::cin >> num1;
+
+  /* ler valor e atribui a num2 */
+  std::cout << "informe outro numero:  \n";
+  int num2 = 0;
+  std::cin >> num2;
+  
+  /* Exibir valores na tela */
+  std::cout << "Os números digitados foram:  " << num1 << " e "  << num2;
+
+  return 0;
 }
