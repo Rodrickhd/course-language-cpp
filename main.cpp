f66a6770d6914b01204f88a2e8e51c5f726b3d2c
Rodrickhd <brodrigo87@gmail.com>
2020-07-28 13:48:37 +0000

    Incremento e Decremento

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 /*
  * Copyright (c) 2020 Rodrigo Barros. All rights reserved.
- * Estrutura FOR (Aula 27)
+ * Operador de incremento e Decremento (Aula 28)
  */
  
+ // Operador de Incremento = ++    (x++;)
+ // Operador de Decremento = --    (x=x-1;)
 #include <iostream>
 
 int main()  {
 
+  //int i = 0;
+  //i--;
+  //std::cout << i << "\n";
 
- for(int i = 0; i<=10; i++) {
- std::cout << "O valor de i é igual : " << i << "\n";
+
+  //Pré Incremento  =  ++x;
+  //Pós Incremento  =  x++;
+
+
+  int i = 0, i2 = 0;
+  std::cout << "Pré " << ++i << "\n";  //pré-incrementar
+  std::cout << "Pós " << i2++ << "\n"; //pos-incrementar
+  std::cout << "Pós " << i2 << "\n";
+  
+  std::cout << "\n";
+
+  std::cout << "Operador de decremento " << "\n";
  
- }
-
-
-
+  i = 0; 
+  i2 = 0;
+  std::cout << "Pré " << --i << "\n";   //pré-decrementar
+  std::cout << "Pós " << i2-- << "\n"; //pos-decrementar
+  std::cout << "Pós " << i2 << "\n";   
 
 }
