aea5a29d537ce029da4d5404fe817e65794ebf44
Rodrickhd <brodrigo87@gmail.com>
2020-07-28 13:32:53 +0000

    Estrutura FOR

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 /*
  * Copyright (c) 2020 Rodrigo Barros. All rights reserved.
- * Operadores lógicos
+ * Estrutura FOR (Aula 27)
  */
  
 #include <iostream>
 
 int main()  {
- 
-  std::cout << "Digite um numero no intervalo de 45 até 72 \n " ;
-  int i = 0;
-  std::cin >> i;
 
-  if(i >= 45 && i <= 72){
-    std::cout << "Obrigado por informar o numero " << "no intervalo solicitado \n " ;
-  if(i ==45 || i == 72)
-    std::cout << "O valor de i é igual ao valor de algum extremo! \n";
-}else
-  std::cout << "O numero digitado não está no intervalo solicitado \n" ;
+
+ for(int i = 0; i<=10; i++) {
+ std::cout << "O valor de i é igual : " << i << "\n";
+ 
+ }
 
 
 
