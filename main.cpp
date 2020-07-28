9297b550337f3c177e90b410018ab4face726666
Rodrickhd <brodrigo87@gmail.com>
2020-07-28 13:10:36 +0000

    Operadores lógicos

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 /*
  * Copyright (c) 2020 Rodrigo Barros. All rights reserved.
- * Tomada de decisão 3 (Exemplo com médias de notas de semestre)
+ * Operadores lógicos
  */
  
 #include <iostream>
 
 int main()  {
  
-std::cout << "Digite um numero no intervalo de 45 até 72" << "\n";
-int i = 0;
-std::cin >> i;
+  std::cout << "Digite um numero no intervalo de 45 até 72 \n " ;
+  int i = 0;
+  std::cin >> i;
 
-if(i >= 45 && i <= 72) {
-std::cout << "Obrigado por informar o numero " << " no intervalo solicitado" << "\n" ;
-
- if(i == 45 || i == 72)
- std::cout << "O valor de i é igual ao valor de algum extremo" << "\n";
-
+  if(i >= 45 && i <= 72){
+    std::cout << "Obrigado por informar o numero " << "no intervalo solicitado \n " ;
+  if(i ==45 || i == 72)
+    std::cout << "O valor de i é igual ao valor de algum extremo! \n";
 }else
-  std::cout << "O numero digitado não está no intervalo solicitado" << "\n";
+  std::cout << "O numero digitado não está no intervalo solicitado \n" ;
+
+
 
 
 }
-  
-
-
